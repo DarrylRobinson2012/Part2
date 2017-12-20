a3ac60cd3cd30b2cddd176c8149f1f5cfb385afc
DarrylRobinson2012 <darry.robinson2012@gmail.com>
2017-12-19 20:00:24 -0500

    added two structs to the project

--- unit2remake/QuestionProvider .swift
+++ unit2remake/QuestionProvider .swift
@@ -10,7 +10,33 @@ import Foundation
 import GameKit
 
 
-struct Trivia {
+struct TriviaQuestions {
+    var questions : String
+    var answers = [String]()
+    var correctAnswer = Int()
+    
+}
 
+
+struct QuestionData {
+    // Using the TriviaQuestions struct the array questionsForUser is created.
+    let questionsForUser: [TriviaQuestions] = [ TriviaQuestions(questions: "Which car is the fastest",
+                                                               answers: ["Charger","Mustang", "E350", "M4"],
+                                                               correctAnswer: 0),
+                                                TriviaQuestions(questions: "Which car was made in Europe",
+                                                                answers: ["pinto","Challenger", "Mercedes", " Toyota"],
+                                                                correctAnswer: 2),
+                                                TriviaQuestions(questions: "Which car is the most expensive",
+                                                                    answers: ["Explorer","Blazer", "Raptor", "G-Wagon"],
+                                                                    correctAnswer: 3),
+                                                TriviaQuestions(questions: "Which car has doors that open upward",
+                                                                    answers: ["Impala","Eclipse", "corvette", "Lambo"],
+                                                                    correctAnswer: 3),
+                                                TriviaQuestions(questions: "Which Car has American muscle",
+                                                                answers: ["Challenger","Mustang", "Cammaro", "All of the above"],
+                                                                correctAnswer: 3),
+                                                TriviaQuestions(questions: "Which car is mad for off road obstacles",
+                                                                answers: ["corolla","jeep", "taruas", "Lexus"],
+                                                                correctAnswer: 1)]
     
 }
