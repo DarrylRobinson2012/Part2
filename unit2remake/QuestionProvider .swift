88e6b969f23be6708212a26b753f7bf6244d7328
DarrylRobinson2012 <darry.robinson2012@gmail.com>
2017-12-19 19:08:08 -0500

    Updated user interface with Auto layout

---
+++ unit2remake/QuestionProvider .swift
@@ -0,0 +1,16 @@
+//
+//  QuestionProvider .swift
+//  unit2remake
+//
+//  Created by Darryl Robinson  on 12/19/17.
+//  Copyright © 2017 DrobEnterprises. All rights reserved.
+//
+
+import Foundation
+import GameKit
+
+
+struct Trivia {
+
+    
+}
